6cd9e74e6a58bf81235d7c8cc71226c94ab8475a
Alexey <dr.a.lekter@gmail.com>
2020-10-12 23:35:58 +0300

    Начал 10 урок: стр. категории

---
+++ src/components/CategoryCreate.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="col s12 m6">
+    <div>
+      <div class="page-subtitle">
+        <h4>Создать</h4>
+      </div>
+
+      <form>
+        <div class="input-field">
+          <input
+            id="name"
+            type="text"
+            v-model="title"
+            :class="{invalid: $v.title.$dirty && !$v.title.required}"
+          >
+          <label for="name">Название</label>
+          <span
+            v-if="$v.title.$dirty && !$v.title.required"
+            class="helper-text invalid"
+            >
+            Введите название категории
+          </span>
+        </div>
+
+        <div class="input-field">
+          <input
+            id="limit"
+            type="number"
+            v-model="limit"
+            :class="{invalid: $v.limit.$dirty && !$v.limit.minValue}"
+          >
+          <label for="limit">Лимит</label>
+          <span
+            v-if="$v.limit.$dirty && !$v.limit.minValue"
+            class="helper-text invalid"
+          >
+            Минимальная величина
+          </span>
+        </div>
+
+        <button class="btn waves-effect waves-light" type="submit">
+          Создать
+          <i class="material-icons right">send</i>
+        </button>
+      </form>
+    </div>
+  </div>
+</template>
+
+<script>
+import { required, minValue } from 'vuelidate/lib/validators';
+export default {
+  date: () => ({
+    title: '',
+    limit: 1
+  }),
+  validations: {
+    title: {required},
+    limit: {minValue: minValue(1)}
+  }
+}
+</script>
